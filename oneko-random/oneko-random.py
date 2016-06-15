0cc38b44737456efa6a8a61b2a6fe63bd1b241bb
Giovanny Garciliano Diaz <xivanni@hotmail.com>
2016-06-15 18:55:11 -0500

    Adding a optimization, adding a Popen

--- oneko-random/oneko-random.py
+++ oneko-random/oneko-random.py
@@ -1,68 +1,70 @@
+from subprocess import Popen
 from subprocess import call
 from random import randint
 import sys
 arg = sys.argv
-''' Checking system
 if sys.platform != "linux2":
-    print ("linux2")
-    exit (2) # Error 2: oneko-random only supports Linux systems'''
+	print ("oneko-random detects your system isn't linux")
 # Character
 char = randint(1,5)
 if char == 1:
-    char = "-tora"
+	char = "-tora"
 elif char == 2:
-    char = "-dog"
+	char = "-dog"
 elif char == 3:
-    char = "-sakura"
+	char = "-sakura"
 elif char == 4:
-    char = "-tomoyo"
+	char = "-tomoyo"
 elif char == 5:
-    char = "-neko"
-# Background Color
-try:
-    if arg.index("-c") or arg.index("--bgcolor"):
-	bgcolor = randint(1,5)
-    if bgcolor == 1:
-        bgcolor = "-bg red"
-    elif bgcolor == 2:
-        bgcolor = "-bg green"
-    elif bgcolor == 3:
-        bgcolor = "-bg blue"
-    elif bgcolor == 4:
-        bgcolor = "-bg black"
-    elif bgcolor == 5:
-        bgcolor = "-bg white"
-except:
-    bgcolor = ""
-# Foreground Color
-try:
-    if arg.index("-f") or arg.index("--fgcolor"):
-	fgcolor = randint(1,5)
-    if fgcolor == 1:
-        fgcolor = "-fg red"
-    elif fgcolor == 2:
-    	fgcolor = "-fg green"
-    elif fgcolor == 3:
-	fgcolor = "-fg blue"
-    elif fgcolor == 4:
-	fgcolor = "-fg black"
-    elif fgcolor == 5:
-	fgcolor = "-fg white"
-except:
-	fgcolor = ""
+	char = "-neko"
 # Invert Color
 try:
-    if arg.index("-v") or arg.index("--rvideo"):
-        rvideo = randint(0,1)
-	rvideo = "-rv"
+	if arg.index("-v") or arg.index("--rvideo"):
+		rvideo = randint(0,1)
+	if rvideo == 1:
+		rvideo = "-rv&"
+		command = ["oneko",char,rvideo]
+	else:
+		command = ["oneko",char]
 except:
-    rvideo = ""
-# Create command
-command = ["oneko",char,bgcolor,fgcolor,rvideo]
+	rvideo = ""
+# Background Color
 try:
-    print (command)
-    call(command)
-    exit(0)
+	if arg.index("-c") or arg.index("--bgcolor"):
+		bgcolor = randint(1,5)
+	if bgcolor == 1:
+		bgcolor = "-bg red"
+	elif bgcolor == 2:
+		bgcolor = "-bg green"
+	elif bgcolor == 3:
+		bgcolor = "-bg blue"
+	elif bgcolor == 4:
+		bgcolor = "-bg black"
+	elif bgcolor == 5:
+		bgcolor = "-bg white"
 except:
-    print ("oneko")
-    exit(1) # Error 1: Oneko not installed in the system
+	bgcolor = "-fg white"
+# Foreground Color
+try:
+	if arg.index("-f") or arg.index("--fgcolor"):
+		fgcolor = randint(1,5)
+	if fgcolor == 1:
+		fgcolor = "-fg red"
+	elif fgcolor == 2:
+		fgcolor = "-fg green"
+	elif fgcolor == 3:
+		fgcolor = "-fg blue"
+	elif fgcolor == 4:
+		fgcolor = "-fg black"
+	elif fgcolor == 5:
+		fgcolor = "-fg white"
+except:
+	fgcolor = "-fg black"
+# Create command
+try:
+	command = Popen(["oneko",char,bgcolor,fgcolor,rvideo,"&"])
+	command.wait()
+	exit(0)
+except:
+	print ("oneko")
+	exit(1) # Error 1: Oneko not installed in the system
