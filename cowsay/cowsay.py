85775d3fc79721a7a59fe811da78c81e12daf6bc
Giovanny Garciliano Diaz <xivanni@hotmail.com>
2016-09-26 07:39:46 -0500

    Adding os acknowledgement

--- cowsay/cowsay.py
+++ cowsay/cowsay.py
@@ -3,7 +3,7 @@ from subprocess import Popen
 from subprocess import PIPE
 argc=len(sys.argv)
 def funcConsole(a, b):
-    try:
+	try:
 		c=open(b,"rb")
 		print ("     /"+"-".center(len(a),"-")+"\\")
 		print ("     |"+a+"|")
@@ -13,23 +13,25 @@ def funcConsole(a, b):
 		print ("         .")
 		#for f in c.readlines():
 		#	print (f)
-		com = ["cat",b]
+		if sys.platform == "win32":
+			com = ["type",b]
+		else:
+			com = ["cat",b]
 		pr = Popen(com, stdout=PIPE)
 		li = pr.stdout.read()
 		print li
 		pr.stdout.close()
 		c.close()
-    except:
+	except:
 		print ("No found file")
 		return 2 # return 2 : File Not Exist
 def funcUser():
-    x=raw_input("Text to show: ")
-    y=raw_input("Path of the file of the picture: ")
-    funcConsole(x,y)
+	x=raw_input("Text to show: ")
+	y=raw_input("Path of the file of the picture: ")
+	funcConsole(x,y)
 if argc==1:
 	funcUser()
 elif argc!=3:
 	sys.exit(1) # return 1 : Incorrect Parameters
 else:
 	funcConsole(sys.argv[1],sys.argv[2])
-
